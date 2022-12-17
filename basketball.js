060ce76b24b466b647ba43ef29e6c956b524b387
siyaa12 <103738367+siyaa12@users.noreply.github.com>
2022-12-17 22:11:14 +0530

    Add files via upload

---
+++ basketball.js
@@ -0,0 +1,35 @@
+let incBtn1=document.getElementById("scr-btn1")
+let incBtn2=document.getElementById("scr-btn2")
+let i=0
+function increment1(){
+    i+=1
+    incBtn1.textContent=i
+}
+// let incBtn2=document.getElementById("scr-btn1")
+function increment2(){
+    i+=2
+    incBtn1.textContent=i
+    
+}
+// let incBtn3=document.getElementById("scr-btn1")
+function increment3(){
+    i+=3
+    incBtn1.textContent=i
+    
+}
+let j=0
+// let incBtn4=document.getElementById("scr-btn2")
+function increment4(){
+    j+=1
+    incBtn2.textContent=j
+}
+// let incBtn5=document.getElementById("scr-btn1")
+function increment5(){
+    j+=2
+    incBtn2.textContent=j
+}
+// let incBtn6=document.getElementById("scr-btn1")
+function increment6(){
+    j+=3
+    incBtn2.textContent=j
+}
